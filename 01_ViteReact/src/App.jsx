ff803a285a7de10bc153534f7bda3b93c240d6d5
Arun Verma <info.arunverma23@gmail.com>
2025-12-04 09:03:57 +0530

    understand React Flow

---
+++ 01_ViteReact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Demo from './Demo'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Mota Bhai</h1>
+      <Demo />
+    </>
+   
+  )
+}
+
+export default App
---
+++ 01_ViteReact/src/Demo.jsx
@@ -0,0 +1,7 @@
+function Print(){
+    return (
+        <h1>Prnt Successfully</h1>
+    )
+}
+
+export default Print
